51e0ce9e940fd969664b8f0ca9cd920178947c52
Christos Keramitsis <c.keramitsis@raphael-gmbh.de>
2022-03-17 14:38:15 +0100

    yoyoyo chat

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ const io = socketIO(server);
 
 io.on('connection', (socket) => {
   console.log('Client connected');
+
+  socket.on('message', (message) => {
+    console.log(message);
+    io.emit('message', `${socket.id.substr(0,2)} said ${message}`);
+  });
+
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
